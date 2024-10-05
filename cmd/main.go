8b962c066c8b87b5d2a674774967d4616e084ea3
Alejandro <alex.mac.fon@gmail.com>
2024-10-05 10:39:56 -0600

    style: indented with tabs

--- cmd/main.go
+++ cmd/main.go
@@ -8,16 +8,16 @@ import (
 )
 
 func main() {
-  router := gin.Default()
-  fmt.Println("Running app")
-  router.LoadHTMLGlob("templates/*")
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  router.GET("/", func(c *gin.Context) {
-	  c.HTML(200, "index.html", gin.H{})
-  })
-  router.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router := gin.Default()
+	fmt.Println("Running app")
+	router.LoadHTMLGlob("templates/*")
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", gin.H{})
+	})
+	router.Run(":8000") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
