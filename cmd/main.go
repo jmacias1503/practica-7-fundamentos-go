f2048c30fd09bb41eefc6663c8a40014dd4d3ef1
Alejandro <alex.mac.fon@gmail.com>
2024-10-08 17:05:36 -0600

    refactor(db): changed from database

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -20,7 +20,7 @@ type User struct {
 
 func main() {
 	dsn := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Error connecting to DB")
 	}
